b39e75f47626b524522864b8dd6565efa39b8dd1
sanskar12k <sanskarmodanwal8@gmail.com>
2021-09-14 21:32:04 +0530

    modified image path

--- AAM.php
+++ AAM.php
@@ -67,7 +67,7 @@
 				<div class="row d-flex justify-content-center">
 					<div class="col-md-6 x">
 					<div class="image">
-					<img  src="img/day1/arena3.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/arena3.jpg" class="img-fluid image__img">
 				      <div class="image__overlay image__overlay--primary text-center">
 							<div class="image__title">Arena</div>
 							<p class="image__description">The Arena is a nostalgia-filled site welcoming the Alumni to share their old memories with us.</p>	
@@ -75,22 +75,22 @@
 			</div>
 			<div class="col-md-3 x align-items-center">
 					<div class="image">
-					<img  src="img/day2/arena.jpg" class="img-fluid image__img">
+					<img  src="img/day2c/compressed day 2/arena.jpg" class="img-fluid image__img">
 				      <div class="image__overlay image__overlay--primary text-center">
 					</div></div>
 				<div class="image pt-3">
-					<img  src="img/day2/cake_cutting.jpg" class="img-fluid image__img">
+					<img  src="img/day2c/compressed day 2/cake_cutting.jpg" class="img-fluid image__img">
 				      <div class="image__overlay image__overlay--primary text-center">
 					  </div></div>
 				
 			</div>
 			<div class="col-md-3 x align-items-center">
 					<div class="image">
-					<img  src="img/day1/alumni4.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/alumni4.jpg" class="img-fluid image__img">
 				      <div class="image__overlay image__overlay--primary text-center">
 						</div></div>
 				<div class="image pt-3">
-					<img  src="img/day1/alumni2.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/alumni2.jpg" class="img-fluid image__img">
 				      <div class="image__overlay image__overlay--primary text-center">
 					  </div></div>
 			</div>
@@ -115,11 +115,11 @@
 			<div class="col-md-4 x align-items-center">
 					
 				<div class="image ">
-					<img  src="img/day1/awards.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/awards.jpg" class="img-fluid image__img">
 				      <div class="image__overlay image__overlay--primary text-center">
 								</div></div>
 				<div class="image pt-3">
-					<img  src="img/day1/alumni.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/alumni.jpg" class="img-fluid image__img">
 				      <div class="image__overlay image__overlay--primary text-center">
 					  
 							</div></div>
@@ -127,7 +127,7 @@
 			</div>
 			<div class="col-md-8 x">
 				<div class="image">
-				<img  src="img/day3/ceremony4.jpg" class="img-fluid image__img">
+				<img  src="img/day3c/compressed day 3/ceremony4.jpg" class="img-fluid image__img">
 				<div class="image__overlay image__overlay--primary text-center">
 				<div class="image__title">Grand Reunion</div>
 							<p class="image__description">The Annual Alumni Meet is a fascinating trip down memory lane
@@ -155,18 +155,18 @@
 			<div class="row d-flex justify-content-center">
 			<div class="col-md-4 x align-items-center">
 					<div class="image">
-					<img  src="img/day2/nalanda2.jpg" class="img-fluid image__img">
+					<img  src="img/day2c/compressed day 2/nalanda2.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary text-center">
 				</div></div>
 				<div class="image pt-3">
-					<img  src="img/day1/alumni.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/alumni.jpg" class="img-fluid image__img">
 				      <div class="image__overlay image__overlay--primary text-center">
 					</div></div>
 				
 			</div>
 			<div class="col-md-8 x">
 				<div class="image">
-					<img  src="img/day1/dept_visit2.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/dept_visit2.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary text-center">
 							<div class="image__title">Department Visit</div>
 							<p class="image__description">Departments are our own shrine of knowledge filled with bittersweet 
@@ -199,7 +199,7 @@
 				</div>
 				<div class="col-md-6 content-overlay x">	
 				<div class="image">
-					<img  src="img/day2/yellow-diary.jpg" class="img-fluid image__img">
+					<img  src="img/day2c/compressed day 2/yellow-diary.jpg" class="img-fluid image__img">
 				</div>
 				</div>
 									
@@ -222,28 +222,28 @@
 			<div class="row d-flex justify-content-center">
 				<div class="col-md-3 content-overlay x">					
 					<div class="image ">
-					<img  src="img/day1/flames.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/flames.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 				</div></div>				
 					<div class="image pt-3">
-					<img  src="img/day1/hall_visit2.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/hall_visit2.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 				</div></div>
 </div>
 				<div class="col-md-3 content-overlay x">					
 					<div class="image ">
-					<img  src="img/day1/flames2.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/flames2.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 				</div></div>				
 					<div class="image pt-2">
-					<img  src="img/day1/hall_visit4.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/hall_visit4.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 				</div></div></div>
 				
 				<div class="col-md-6 content-overlay x">
 										<div class="image">
 
-					<img src="img/day1/hall_visit.jpg" class="img-fluid image__img">
+					<img src="img/day1c/compressed day 1/hall_visit.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary text-center">
 							<div class="image__title">Hall Visit</div>
 							<p class="image__description">The place we learnt and lived for the 4 most important years in our life surely has a fragment of our soul inside
@@ -270,7 +270,7 @@
 			<div class="row d-flex justify-content-center">			
 			<div class="col-md-7 content-overlay x">
 				<div class="image">
-					<img  src="img/day2/campus_tour.jpg" class="img-fluid image__img">
+					<img  src="img/day2c/compressed day 2/campus_tour.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary text-center">
 													<div class="image__title">Campus Tour</div>
 							<p class="image__description">Campus Tour is our way of showing the 2200 acre campus in a nutshell
@@ -280,7 +280,7 @@
 				<div class="col-md-5 content-overlay x">
 									<div class="image">
 
-					<img  src="img/day2/campus_tour2.jpg" class="img-fluid image__img">
+					<img  src="img/day2c/compressed day 2/campus_tour2.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary text-center">
 									<div class="image__title">Campus Tour</div>
 							<p class="image__description"></p>	
@@ -290,7 +290,7 @@
 	<div class="row d-flex justify-content-center pt-3">	
 	<div class="col-md-5 x">
 					<div class="image">
-					<img  src="img/day2/nalanda.jpg" class="img-fluid image__img">
+					<img  src="img/day2c/compressed day 2/nalanda.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary text-center text-center">
 								<div class="image__title">Campus Tour</div>
 							<p class="image__description">
@@ -299,7 +299,7 @@
 				</div>
 				<div class="col-md-7 x">
 					<div class="image">
-					<img src="img/day2/football.jpg" class="img-fluid image__img">
+					<img src="img/day2c/compressed day 2/football.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 					<div class="image__title">Campus Tour</div>
 							<p class="image__description">In the progressive world we live in, there are a lot of changes and yet a lot of constants 
@@ -325,7 +325,7 @@
 			<div class="row d-flex justify-content-center">
 				<div class="col-md-8 content-overlay x">	
 					<div class="image">
-					<img src="img/day1/akash2.jpg" class="img-fluid image__img">
+					<img src="img/day1c/compressed day 1/akash2.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary text-center">
 						<div class="image__title">Stand-Up Comedy</div>
 							<p class="image__description">Smiles can cast the most powerful spell of love and happiness.</p>	
@@ -333,12 +333,12 @@
 		</div>
 		<div class="col-md-4 content-overlay x">					
 				<div class="image">
-					<img  src="img/day1/toat.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/toat.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 					
 				</div></div>
 				<div class="image pt-3">
-					<img  src="img/day1/toat2.jpg" class="img-fluid image__img">
+					<img  src="img/day1c/compressed day 1/toat2.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 					
 				</div></div></div>
@@ -364,11 +364,11 @@
 			<div class="row d-flex justify-content-center">
 		<div class="col-md-4 content-overlay x">					
 			<div class="image">
-					<img  src="img/day2/illu1.jpg" class="img-fluid image__img">
+					<img  src="img/day2c/compressed day 2/illu1.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 				</div></div>
 				<div class="image pt-3">
-					<img  src="img/day2/lantern.jpg" class="img-fluid image__img">
+					<img  src="img/day2c/compressed day 2/lantern.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 							<div class="image__title">Illumination</div>
 							<p class="image__description"></p>	
@@ -377,7 +377,7 @@
 <div class="col-md-8 content-overlay x">
 										<div class="image">
 
-					<img src="img/day2/kgp_arena.jpg" class="img-fluid image__img">
+					<img src="img/day2c/compressed day 2/kgp_arena.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary text-center">
 				<div class="image__title">Illumination</div>
 							<p class="image__description">The Illumination with diyas followed by the release of sky lanterns 
@@ -388,14 +388,14 @@
 				<div class="row d-flex justify-content-center pt-3">
 				<div class="col-md-4 content-overlay x">
 					<div class="image">
-					<img  src="img/day2/night1.jpg" class="img-fluid image__img">
+					<img  src="img/day2c/compressed day 2/night1.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 							<div class="image__title">Gala Dinner</div>
 				</div></div>
 				</div>
 				<div class="col-md-4 content-overlay x">
 				<div class="image">
-					<img  src="img/day2/night2.jpg" class="img-fluid image__img">
+					<img  src="img/day2c/compressed day 2/night2.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 							<div class="image__title">Gala Dinner</div>
 				</div></div>
@@ -403,7 +403,7 @@
 				<div class="col-md-4 content-overlay x">
 										<div class="image">
 
-					<img  src="img/day2/night3.jpg" class="img-fluid image__img">
+					<img  src="img/day2c/compressed day 2/night3.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 							<div class="image__title">Gala Dinner</div>
 				</div></div>
@@ -429,14 +429,14 @@
 			<div class="row d-flex justify-content-center">
 					<div class="col-md-6 content-overlay x">
 					<div class="image">
-					<img  src="img/day3/emo.jpg" class="img-fluid image__img">
+					<img  src="img/day3c/compressed day 3/emo.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 				</div></div>
 				</div>
 				<div class="col-md-6 content-overlay x">
 									<div class="image">
 
-					<img  src="img/day3/marathon.jpg" class="img-fluid image__img">
+					<img  src="img/day3c/compressed day 3/marathon.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary text-center">
 							<div class="image__title">2.2 Marathon</div>
 							<p class="image__description">The iconic 2.2 km marathon around the heart of the campus in which all 
@@ -448,7 +448,7 @@
 					<div class="col-md-8 content-overlay">
 										<div class="image">
 
-					<img src="img/day3/closing_ceremony.jpg" class="img-fluid image__img">
+					<img src="img/day3c/compressed day 3/closing_ceremony.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 							<div class="image__title">Closing Ceremony</div>	
 				</div></div>
--- yearbook.php
+++ yearbook.php
@@ -93,16 +93,16 @@
                                                 <div class="card" style="height: 100% !important;">
                                                     <img src="img\yearbook\year2020.jpg" class="h-img">
                                                     <div class="card-body">
-                                                        <h4 class="card-title">YearBook 2020...</h4>
+                                                        <h4 class="card-title">Yearbook 2020...</h4>
                                                         <div class="row ">
                                                             <div class="img-txt ">
-                                                                <h6>UG</h6>
+                                                               <a href="https://drive.google.com/file/d/1v8SiIwCqeIzERMoYXC9bxKQu7pPz3muP/view?usp=sharing"  target="_blank" style="color:black"> UG</a> 
                                                             </div>
                                                             <div class="img-txt">
-                                                                <h6>PG</h6>
+                                                            <a href="https://drive.google.com/file/d/1qhRMxn9StnsPWs1C1-f6gfllCViSYlnB/view?usp=sharing "target="_blank" style="color:black"> PG</a>
                                                             </div>
                                                             <div class="img-txt">
-                                                                <h6>RS</h6>
+                                                            <a href="https://drive.google.com/file/d/1jelTjKSsrS7LnZDniAiC4yqjeZ-yOjgb/view?usp=sharing "target="_blank" style="color:black"> RS</a>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -115,13 +115,13 @@
                                                         <h4 class="card-title">YearBook 2019...</h4>
                                                         <div class="row ">
                                                             <div class="img-txt ">
-                                                                <h6>UG</h6>
+                                                               <a href="https://drive.google.com/file/d/1h0Opa3GNGR20Hpiw71EwhwwjcDIwj7DJ/view?usp=sharing" target="_blank" style="color:black"> UG</a>
                                                             </div>
                                                             <div class="img-txt">
-                                                                <h6>PG</h6>
+                                                            <a href="https://drive.google.com/file/d/1h0Opa3GNGR20Hpiw71EwhwwjcDIwj7DJ/view?usp=sharing" target="_blank" style="color:black"> PG</a>
                                                             </div>
                                                             <div class="img-txt">
-                                                                <h6>RS</h6>
+                                                            <a href="https://drive.google.com/file/d/1h0Opa3GNGR20Hpiw71EwhwwjcDIwj7DJ/view?usp=sharing" target="_blank" style="color:black"> RS</a>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -140,13 +140,13 @@
                                                         <h4 class="card-title">YearBook 2018...</h4>
                                                         <div class="row ">
                                                             <div class="img-txt ">
-                                                                <h6>UG</h6>
+                                                               <a href="https://drive.google.com/file/d/1WvuLhTKNVHc-owJhFxJS5CWLjBfKDrC1/view?usp=sharing"target="_blank" style="color:black">UG</a>
                                                             </div>
                                                             <div class="img-txt">
-                                                                <h6>PG</h6>
+                                                            <a href="https://drive.google.com/file/d/1WvuLhTKNVHc-owJhFxJS5CWLjBfKDrC1/view?usp=sharing"target="_blank" style="color:black">PG</a>
                                                             </div>
                                                             <div class="img-txt">
-                                                                <h6>RS</h6>
+                                                            <a href="https://drive.google.com/file/d/1WvuLhTKNVHc-owJhFxJS5CWLjBfKDrC1/view?usp=sharing"target="_blank" style="color:black">RS</a>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -159,13 +159,13 @@
                                                         <h4 class="card-title">YearBook 2017...</h4>
                                                         <div class="row ">
                                                             <div class="img-txt ">
-                                                                <h6>UG</h6>
+                                                               <a href="https://drive.google.com/file/d/0ByPZPiiPCG8_aFFtN1VRRU5lb00/view?usp=sharing&resourcekey=0-wity-gekXThY6oNZ4Iya4A"target="_blank" style="color:black"> UG</a>
                                                             </div>
                                                             <div class="img-txt">
-                                                                <h6>PG</h6>
+                                                            <a href="https://drive.google.com/file/d/0ByPZPiiPCG8_aC1kaHRyNDEtUG8/view?usp=sharing&resourcekey=0-gvQo-b-rsfrSRYFqpjM-ZA"target="_blank" style="color:black"> PG</a>
                                                             </div>
                                                             <div class="img-txt">
-                                                                <h6>RS</h6>
+                                                           <a href="https://drive.google.com/file/d/0ByPZPiiPCG8_d2ZfVGw5TTNMekE/view?usp=sharing&resourcekey=0-QyxYHUpYEfDgNLi3txyZ1w" target="_blank" style="color:black">RS</a>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -178,13 +178,13 @@
                                                         <h4 class="card-title">YearBook 2016...</h4>
                                                         <div class="row ">
                                                             <div class="img-txt ">
-                                                                <h6>UG</h6>
+                                                               <a href="https://drive.google.com/file/d/0B2DrxeNIhpL0dGNlc0RpYjhXcFk/view?ts=59fd5ae5&resourcekey=0-tv1hLGs35PN1uAhH8ZqvMQ" target="_blank" style="color:black">UG</a>
                                                             </div>
                                                             <div class="img-txt">
-                                                                <h6>PG</h6>
+                                                            <a href="https://drive.google.com/file/d/0B2DrxeNIhpL0dGNlc0RpYjhXcFk/view?ts=59fd5ae5&resourcekey=0-tv1hLGs35PN1uAhH8ZqvMQ" target="_blank" style="color:black">PG</a>
                                                             </div>
                                                             <div class="img-txt">
-                                                                <h6>RS</h6>
+                                                            <a href="https://drive.google.com/file/d/0B2DrxeNIhpL0dGNlc0RpYjhXcFk/view?ts=59fd5ae5&resourcekey=0-tv1hLGs35PN1uAhH8ZqvMQ" target="_blank" style="color:black">RS</a>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -203,13 +203,13 @@
                                                         <h4 class="card-title">YearBook 2015...</h4>
                                                         <div class="row ">
                                                             <div class="img-txt ">
-                                                                <h6>UG</h6>
+                                                              <a href="https://drive.google.com/file/d/0B29Q9s5AMUD7Zl83cklZdEV5bzQ/view?ts=59fd6a04&resourcekey=0-wfnygWgzFZqUTMIQCDcrnQ" target="_blank" style="color:black">UG</a>
                                                             </div>
                                                             <div class="img-txt">
-                                                                <h6>PG</h6>
+                                                            <a href="https://drive.google.com/file/d/0B29Q9s5AMUD7Zl83cklZdEV5bzQ/view?ts=59fd6a04&resourcekey=0-wfnygWgzFZqUTMIQCDcrnQ"target="_blank" style="color:black">PG</a>
                                                             </div>
                                                             <div class="img-txt">
-                                                                <h6>RS</h6>
+                                                            <a href="https://drive.google.com/file/d/0B29Q9s5AMUD7Zl83cklZdEV5bzQ/view?ts=59fd6a04&resourcekey=0-wfnygWgzFZqUTMIQCDcrnQ"target="_blank" style="color:black">RS</a>
                                                             </div>
                                                         </div>
                                                     </div>
